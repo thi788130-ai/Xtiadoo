bf776530ac8f6a29932972aec7878a9c82f44e5c
thi788130-ai <thi788130@gmail.com>
2026-04-15 16:15:55 -0300

    Add files via upload

---
+++ service-worke.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = "overlay-pro-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+// INSTALAR
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsTo
